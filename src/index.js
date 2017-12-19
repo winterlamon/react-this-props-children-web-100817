44f6ae0c205e40d53e57a52b0044fbfbbe1694cd
github username <github email address>
2017-12-19 12:53:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Code Goes Here
+import React from 'react';
+
+export default class SomeComponent extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="some-component-special-class">{child}</div>
+      );
+    });
+
+    return (
+      <div className="some-component">
+        <p>This component has {React.Children.count(this.props.children)} children.</p>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
